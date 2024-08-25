7e44635e721ebb6d0816950715ecb97b2b891b29
Muhammed Ashfaq <123805225+muhammedashfaq@users.noreply.github.com>
2024-08-25 16:08:20 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3xl font-bold">Hello </h1>
+      </div>
+
+    </section>
+ 
+  );
+}
